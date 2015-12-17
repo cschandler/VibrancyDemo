76f2b3bd099dc049e93161d55a4d2caf37a1545b
Charles Chandler <charles.chandler@willowtreeapps.com>
2015-12-17 17:41:54 -0500

    Initial commit

--- VibrancyDemo/ViewController.swift
+++ VibrancyDemo/ViewController.swift
@@ -8,18 +8,54 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    // MARK: UITableViewDatasource & Delegate
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
     }
-
-
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        switch indexPath.row {
+        case 0:
+            let cell = tableView.dequeueReusableCellWithIdentifier("Cell1", forIndexPath: indexPath)
+            return cell
+        case 1:
+            let cell = tableView.dequeueReusableCellWithIdentifier("Cell2", forIndexPath: indexPath)
+            return cell
+        case 2:
+            let cell = tableView.dequeueReusableCellWithIdentifier("Cell3", forIndexPath: indexPath)
+            return cell
+        case 3:
+            let cell = tableView.dequeueReusableCellWithIdentifier("Cell4", forIndexPath: indexPath)
+            return cell
+        case 4:
+            let cell = tableView.dequeueReusableCellWithIdentifier("Cell5", forIndexPath: indexPath)
+            return cell
+        case 5:
+            let cell = tableView.dequeueReusableCellWithIdentifier("Cell6", forIndexPath: indexPath)
+            return cell
+        case 6:
+            let cell = tableView.dequeueReusableCellWithIdentifier("Cell7", forIndexPath: indexPath)
+            return cell
+        case 7:
+            let cell = tableView.dequeueReusableCellWithIdentifier("Cell8", forIndexPath: indexPath)
+            return cell
+        case 8:
+            let cell = tableView.dequeueReusableCellWithIdentifier("Cell9", forIndexPath: indexPath)
+            return cell
+        case 9:
+            let cell = tableView.dequeueReusableCellWithIdentifier("Cell10", forIndexPath: indexPath)
+            return cell
+        default:
+            return UITableViewCell(style: .Default, reuseIdentifier: "Cell1")
+        }
+    }
 }
 
