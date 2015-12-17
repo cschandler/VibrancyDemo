5e9712767de555015e52811f3fa825fa3753371e
Charles Chandler <charles.chandler@willowtreeapps.com>
2015-12-17 14:19:44 -0500

    Initial Commit

---
+++ VibrancyDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VibrancyDemo
+//
+//  Created by Charles Chandler on 12/17/15.
+//  Copyright © 2015 WillowTree, Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
